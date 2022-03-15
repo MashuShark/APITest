ca7d7fde0c6fe08d18be2a813518cc520bdab6a4
MashuShark <us1gfw.maryna@gmail.com>
2022-03-15 15:37:00 +0200

    Refactor ExcelUtils

--- src/test/java/utils/ExcelUtils.java
+++ src/test/java/utils/ExcelUtils.java
@@ -10,43 +10,35 @@ import java.util.Objects;
 
 public class ExcelUtils {
 
-    static String excelPath = "src/main/resources/data.xlsx";
+    static XSSFWorkbook workbook;
+    static XSSFSheet sheet;
 
-    public static XSSFSheet getSheet() {
+    public ExcelUtils(String excelPath, String sheetName){
         try {
-            XSSFWorkbook workbook = new XSSFWorkbook(excelPath);
-            XSSFSheet sheet = workbook.getSheet("Sheet1");
-            return sheet;
+            workbook = new XSSFWorkbook(excelPath);
+            sheet = workbook.getSheet(sheetName);
 
         } catch (Exception exp) {
             System.out.println(exp.getMessage());
-            System.out.println(exp.getCause());
             exp.printStackTrace();
         }
-         return null;
-    }
-
-    public static void main(String[] args) throws IOException {
-        getRowCount();
-        getFirstNameFromCell();
-        getPositionFromCell();
     }
 
     public static void getFirstNameFromCell(){
-        String value = getSheet().getRow(1).getCell(0).getStringCellValue();
+        String value = sheet.getRow(1).getCell(0).getStringCellValue();
         System.out.println("Value from (1,0) Cell -> " + value);
     }
 
     public static void getPositionFromCell(){
         DataFormatter formatter = new DataFormatter();
-        XSSFCell dataCell = getSheet().getRow(1).getCell(4);
+        XSSFCell dataCell = sheet.getRow(1).getCell(4);
         Object value = formatter.formatCellValue(dataCell);
 
         System.out.println("Position from (1,4) Cell is -> " + value);
     }
 
     public static void getRowCount() {
-        int rowCount = Objects.requireNonNull(getSheet()).getPhysicalNumberOfRows();
+        int rowCount = sheet.getPhysicalNumberOfRows();
         System.out.println("Number of rows is -> " + rowCount);
     }
 }
