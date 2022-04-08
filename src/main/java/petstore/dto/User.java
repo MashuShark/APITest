d560986f8cb0fd1eb664fcd7fc9bb3c96201d8b2
MashuShark <us1gfw.maryna@gmail.com>
2022-04-08 12:54:21 +0300

    Rewrite the petstore.dto/User to make it immutable

--- src/main/java/petstore/dto/User.java
+++ src/main/java/petstore/dto/User.java
@@ -26,66 +26,33 @@ public class User {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getUserName() {
         return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public Integer getUserStatus() {
         return userStatus;
     }
-
-    public void setUserStatus(Integer userStatus) {
-        this.userStatus = userStatus;
-    }
-
 
     public static class UserBuilder {
 
